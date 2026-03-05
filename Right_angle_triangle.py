de950d10b7ae8ed34425bfad3a3652f89daa03a6
Hridita-11 <hridita.nz@gmail.com>
2026-03-05 19:12:25 +1300

    Add files via upload

---
+++ Right_angle_triangle.py
@@ -0,0 +1,7 @@
+print("Half pyramid patterns of the star(*):")
+n= int(input("enter the number of the rows:"))
+for i in range(n):
+    for j in range(i+1):
+        print("*",end="")
+    print()
+  
